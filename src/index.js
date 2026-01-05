62968b17c6296c311acf570fe537f996fd03e64b
Cauã Ferreira <185573157+developerferreira@users.noreply.github.com>
2026-01-05 18:30:20 -0300

    mudança no estilo

--- src/index.js
+++ src/index.js
@@ -5,23 +5,3 @@ function abrirProjeto(projeto) {
         window.location.href = 'src/projects/insta_scraper/index.html'
     }
 }
-
-let dark__mode = localStorage.getItem('darkmode')
-const theme__switch = document.querySelector('.theme__switch')
-
-const enableDarkmode = () => {
-    document.body.classList.add('dark__mode')
-    localStorage.setItem('darkmode', 'active')
-}
-
-const disableDarkmode = () => {
-    document.body.classList.remove('dark__mode')
-    localStorage.setItem('darkmode', null)
-}
-
-if(dark__mode === "active") enableDarkmode()
-
-theme__switch.addEventListener("click", () => {
-    dark__mode = localStorage.getItem('darkmode')
-    dark__mode !== "active" ? enableDarkmode() : disableDarkmode()
-})
