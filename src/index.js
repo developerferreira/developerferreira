ebbe912d8b51aeed65f20677a7c84f3def7018dd
Cauã Ferreira <185573157+developerferreira@users.noreply.github.com>
2026-02-18 21:38:29 -0300

    fix insta scraper redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function abrirProjeto(projeto) {
     if (projeto === 'userauth') {
         window.location.href = 'src/projects/userauth/index.html'
-    } else if (projeto === 'insta_scraper') {
+    } else if (projeto === 'insta__scraper') {
         window.location.href = 'src/projects/insta_scraper/index.html'
     } else {
         window.location.href = 'src/projects/todo_list/index.html'
