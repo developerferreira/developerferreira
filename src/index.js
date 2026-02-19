e7435608509ed0b583c9f85f36ed99a55809a195
Cauã Ferreira <185573157+developerferreira@users.noreply.github.com>
2026-02-18 21:23:52 -0300

    add new project & fix "projects" div

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 function abrirProjeto(projeto) {
     if (projeto === 'userauth') {
         window.location.href = 'src/projects/userauth/index.html'
-    } else {
+    } else if (projeto === 'insta_scraper') {
         window.location.href = 'src/projects/insta_scraper/index.html'
+    } else {
+        window.location.href = 'src/projects/todo_list/index.html'
     }
 }
