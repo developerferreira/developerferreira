fa2487d8b1a763a83c2529dacf49ea69b16b6d9d
Cauã Ferreira <185573157+developerferreira@users.noreply.github.com>
2026-06-29 09:52:26 -0300

    add uphere

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function abrirProjeto(projeto) {
     if (projeto === 'userauth') {
         window.location.href = 'src/projects/userauth/index.html'
-    } else if (projeto === 'insta__scraper') {
-        window.location.href = 'src/projects/insta_scraper/index.html'
+    } else if (projeto === 'uphere') {
+        window.location.href = 'src/projects/uphere/index.html'
     } else {
         window.location.href = 'src/projects/todo_list/index.html'
     }
